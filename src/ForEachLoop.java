1950a1172da03048ceeb8bdb67b37e2f33ba02d8
Zenith1235789 <mzenith008@gmail.com>
2022-08-25 16:56:36 +0545

    Loops

---
+++ src/ForEachLoop.java
@@ -0,0 +1,12 @@
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3, 4, 5};
+		for(int b : a) {
+			System.out.println(b);
+		}
+
+	}
+
+}
---
+++ src/WhileLoop.java
@@ -0,0 +1,17 @@
+public class WhileLoop {
+	public static void main(String[] args) {
+		int a = 1;
+		while(a <=5) {
+			System.out.println(a);
+			a++;
+		}
+		
+		
+		//DoWhile Loop
+		int b = 10;
+		do{System.out.println(b);
+		b++;}
+		while(b<=15);
+	}
+	
+}
